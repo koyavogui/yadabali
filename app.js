0955415ce8524f46882865bf20dcc5b46d8bff55
koyavogui <micodium3@gmail.com>
2019-03-26 16:50:42 +0000

    essaie

--- app.js
+++ app.js
@@ -6,12 +6,28 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var Datastore = require('nedb')
+const Admin = new Datastore({ filename: 'admin.db', autoload: true })
+const Marchand = new Datastore({ filename: 'marchand.db', autoload: true })
+const Apprenti = new Datastore({ filename: 'apprenti.db', autoload: true })
+const Maitre =  new Datastore({ filename: 'maitre.db', autoload: true })
+const Recette = new Datastore({filenam : 'recette.db', autoload : true})
+//const db = new Datastore({ filename: 'path/to/datafile', autoload: true });
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// configuration des dbs
+app.set('admindb',Admin);
+app.set('marchantdb',Marchand);
+app.set('apprentidb',Apprenti);
+app.set('maitredb',Maitre);
+app.set('recettedb',Recette);
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +37,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/public', express.static('public'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +55,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
+console.log('Nombre de lecture')
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,147 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
+var fs = require('fs');
+var  session  = require('express-session');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Yadabali', nom:'' });
-});
+      res.render('index', { title: 'Yadabali', maitre : 'form_maitre' });
+    });
 
+    router.get('/form', function(requ, res, next) {
+      res.render('form', { title: 'Yadabali', maitre : 'form_maitre' });
+    });
+    //Requette d'ajout enregistrement Maitre Cuisinier
+ 
+    router.post('/query', async (requ, res, next) => {
+
+      const maitredb = requ.app.get('maitredb');
+      maitredb.count({ user : requ.body.user }, function (err, docs) {
+        if (docs > 0) {
+          res.render('form', { title: 'Yadabali', maitre : 'form' });
+        }else
+        {
+          console.log(docs.nom);
+          console.log(requ.body.nom);
+          res.render('maitre', { title: 'Yadabali', maitre : 'form', nom : requ.body.nom });    
+        }
+        
+      });
+      // Telechargement ded l'image
+      /*var filetoupload = requ.body.photo
+      var form = new formidable.IncomingForm();
+      form.parse(req, function (err, fields, files) {
+      
+        var oldpath = files.filetoupload.path;
+        var newpath = '../public/image/' + files.filetoupload.name;
+        console.log(oldpath);
+        console.log(newpath);
+        console.log();
+        fs.rename(oldpath, newpath, function (err) {
+          if (err) throw err;
+          res.write('File uploaded and moved!');
+          res.end();
+        });
+      });
+      var  enregistrement  = {  
+                  nom  : requ.body.nom
+                ,  prenom : requ.body.prenom
+                ,  contact : requ.body.contact
+                ,  commune : requ.body.commune
+                ,  user : requ.body.user
+                ,  password :   requ.body.password
+                ,  photo : requ.body.photo
+                } ;
+                console.log(enregistrement);
+                
+      
+      let doc = await maitredb.insert ( enregistrement)  
+    
+      console.log(doc);
+      if (doc ) {
+        res.render('form_marchand:id');  
+      } else{
+        res.render('form_marchand:echec');
+      }*/
+    })
+ 
+    // requete d'ajout de marchand
+
+    router.post('/query_mhd', async (requ, res, next) => {
+    //telechargement d'image
+      var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+          var oldpath = requ.body.photo_mhd;
+          var newpath = '/image' + requ.body.photo_mhd;
+          fs.rename(oldpath, newpath, function (err) {
+            if (err) throw err;
+            console.log('File uploaded and moved!');
+            res.end();
+          });
+        })
+      var  enregistrement  = {  
+                    nom  : requ.body.nom
+                  ,  prenom : requ.body.prenom
+                  ,  contact : requ.body.contact
+                  ,  commune : requ.body.commune
+                  ,  user : requ.body.user
+                  ,  password :   requ.body.password
+                  ,  photo : requ.body.photo
+                  } ;
+                  console.log(enregistrement);
+                  
+                  
+        const marchantdb = requ.app.get('marchantdb');
+        let doc = marchantdb.insert ( enregistrement) 
+      
+        console.log(doc);
+        if (doc ) {
+          res.render('form_marchand');  
+        } else{
+          res.render('form_marchand:echec');
+        }
+    })
+    // requete d'ajout d'utilisateur qui est apprenti
+    router.post('/query_user', async (requ, res, next) => {
+
+       // Telechargement ded l'image
+       var filetoupload = requ.body.photo
+       var form = new formidable.IncomingForm();
+       form.parse(req, function (err, fields, files) {
+         var oldpath = files.filetoupload.path;
+         var newpath = '../public/image/' + files.filetoupload.name;
+         console.log(oldpath);
+         console.log(newpath);
+         console.log();
+         fs.rename(oldpath, newpath, function (err) {
+           if (err) throw err;
+           res.write('File uploaded and moved!');
+           res.end();
+         });
+       });
+ 
+      var  enregistrement  = {  
+                    nom  : requ.body.nom
+                  ,  prenom : requ.body.prenom
+                  ,  contact : requ.body.contact
+                  ,  commune : requ.body.commune
+                  ,  user : requ.body.user
+                  ,  password :   requ.body.password
+                  ,  photo : requ.body.photo
+                  } ;
+                  console.log(enregistrement);
+                  
+                  
+        const apprentidb = requ.app.get('apprentidb');
+        let doc = await apprentidb.insert ( enregistrement) // Le  rappel est facultatif       
+        //  newDoc est le document nouvellement inséré, y compris son _id
+        //  newDoc n'a pas de clé appelée notToBeSaved puisque sa valeur était indéfinie
+      
+      console.log(doc);
+      /*if (maitre.insert) {
+        
+      }*/
+      res.render('form_utilisateur');
+})
 module.exports = router;
